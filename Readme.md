fd178763b17e62676dc638762509c6859e8a6568
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2023-03-26 22:51:43 +0900

    Add: writeForm, Readme.md

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# 미니프로젝트 - 게시판 만들기
+
+## 사용된 기술
+
+- Spring Boot
+- Spring MVC
+- Spring JDBC
+- MYSQL - SQL
+- thymeleaf 템플릿 엔진
+
+## 아키텍처
+
+```
+                     Spring Core
+                     Spring MVC                   Spring JDBC    MySQL
+브라우저 ---- 요청 ---> Controller ----> Service ----> DAO ----> DB
+        <--- 응답 --- 템플릿 <---           <----         <----
+                      <------------ layer간에 데이터 전송은 DTO -->
+```
+
--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // HTTP 요청을 받아서 응답을 하는 컴포넌트. 스프링부트가 자동으로 Bean으로 생성한다.
@@ -20,7 +21,7 @@ public class BoardController {
     }
 
     @GetMapping("/detail")
-    public String board(
+    public String detail(
             @RequestParam("id") int id
     ) {
         // id에 해당하는 게시물을 읽어온다.
@@ -30,4 +31,22 @@ public class BoardController {
 
     // 게시글 삭제 기능 추가. 관리자는 모든 글 삭제 가능
     // 게시글 수정 기능 추가.
+
+    @GetMapping("/writeForm")
+    public String writeForm() {
+        // 로그인 한 사용자만 글쓰기 가능. 세션에서 로그인 한 정보를 읽어들인다.
+        // 로그인 하지 않았다면 인덱스 페이지로 리다이렉션
+
+        return "writeForm";
+    }
+
+    @PostMapping("/writeOk")
+    public String writeOk(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content
+    ) {
+        return "redirect:/";
+    }
+
+
 }
